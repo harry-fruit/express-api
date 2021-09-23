d6a400aa14e25c6e979dfecfc97b56bbed9b1655
harry-fruit <isaqueduarte17@gmail.com>
2021-09-23 20:33:52 -0300

    Adding requested changes

--- src/modules/main.ts
+++ src/modules/main.ts
@@ -2,7 +2,7 @@ import "module-alias/register";
 import express from "express";
 import { userRouter } from "@/src/modules/user/router";
 
-export const app = express();
+const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
